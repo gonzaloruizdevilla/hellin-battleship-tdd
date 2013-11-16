69295c3a9b5a8a578b58ab6d8694d8e51b8cdc02
Gonzalo Ruiz de Villa <gonzalo.ruizdevilla@adesis.com>
2013-11-16 09:07:28 +0100

    Paso 1: Constantes o conceptos basicos

--- battleshiptdd/lib/juego.js
+++ battleshiptdd/lib/juego.js
@@ -1,4 +1,48 @@
 /*jslint node: true */
 "use strict";
 
-module.exports = {};
+    var //orientaciones
+        HORIZONTAL = "HORIZONTAL",
+        VERTICAL = "VERTICAL",
+        //tableros
+        ROJO = "ROJO",
+        AZUL = "AZUL",
+        //resultados disparos
+        AGUA = "AGUA",
+        TOCADO = "TOCADO",
+        HUNDIDO = "HUNDIDO",
+        GANA = "GANA",
+        //tipo de baro
+        ACORAZADO = "ACORAZADO",
+        SUBMARINO = "SUBMARINO",
+        CORBETA = "CORBETA",
+        LANCHA = "LANCHA",
+        //estados del juego
+        EMPEZANDO = "EMPEZANDO",
+        JUGANDO = "JUGANDO",
+        FINALIZADO = "FINALIZADO",
+        //estados del tablero
+        COLOCANDO = "COLOCANDO",
+        COLOCADO = "COLOCADO";
+
+
+
+module.exports = {
+    HORIZONTAL: HORIZONTAL,
+    VERTICAL: VERTICAL,
+    ROJO: ROJO,
+    AZUL: AZUL,
+    AGUA: AGUA,
+    TOCADO: TOCADO,
+    HUNDIDO: HUNDIDO,
+    GANA: GANA,
+    ACORAZADO: ACORAZADO,
+    SUBMARINO: SUBMARINO,
+    CORBETA: CORBETA,
+    LANCHA: LANCHA,
+    EMPEZANDO: EMPEZANDO,
+    JUGANDO: JUGANDO,
+    FINALIZADO: FINALIZADO,
+    COLOCADO: COLOCADO,
+    COLOCANDO: COLOCANDO
+};
--- battleshiptdd/test/juegoSpec.js
+++ battleshiptdd/test/juegoSpec.js
@@ -8,4 +8,32 @@ describe('juego', function  () {
     it('debe ser algo', function () {
         expect(juego).not.to.be.null;
     });
+
+    it('proporciona direcciones', function () {
+        expect(juego.HORIZONTAL).not.to.be.undefined;
+        expect(juego.VERTICAL).not.to.be.undefined;
+    });
+
+    it('proporciona estados de tablero', function () {
+        expect(juego.COLOCANDO).not.to.be.undefined;
+        expect(juego.COLOCADO).not.to.be.undefined;
+    });
+
+    it('proporciona estados de tablero', function () {
+        expect(juego.EMPEZANDO).not.to.be.undefined;
+        expect(juego.JUGANDO).not.to.be.undefined;
+        expect(juego.FINALIZADO).not.to.be.undefined;
+    });
+
+    it('proporciona colores', function () {
+        expect(juego.ROJO).not.to.be.undefined;
+        expect(juego.AZUL).not.to.be.undefined;
+    });
+
+    it('proporciona barcos', function () {
+        expect(juego.ACORAZADO).not.to.be.undefined;
+        expect(juego.SUBMARINO).not.to.be.undefined;
+        expect(juego.CORBETA).not.to.be.undefined;
+        expect(juego.LANCHA).not.to.be.undefined;
+    });
 });
